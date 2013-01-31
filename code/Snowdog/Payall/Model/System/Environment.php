113778df4766a8e8db42570f1ab271456d1dd809
maciekkossuda <maciek@kossuda.pl>
2013-01-31 22:40:42 +0100

    fix

--- code/Snowdog/Payall/Model/System/Environment.php
+++ code/Snowdog/Payall/Model/System/Environment.php
@@ -1,16 +1,16 @@
 <?php
 class Snowdog_Payall_Model_System_Environment {
-	const PRODUCTION	= 'secure';
-	const SANDBOX		  = 'sandbox';
+  const PRODUCTION	= 'secure';
+  const SANDBOX		  = 'sandbox';
 
-	public function toOptionArray() {
-		return array(
-		  array(
+  public function toOptionArray() {
+    return array(
+      array(
         'value' => PayU_Account_Model_Environment::SANDBOX,
         'label' => Mage::helper('payall')->__('Yes')
       ),
-			array(
-			  'value' => PayU_Account_Model_Environment::PRODUCTION,
+      array(
+        'value' => PayU_Account_Model_Environment::PRODUCTION,
         'label' => Mage::helper('payall')->__('No')
       )
     );
